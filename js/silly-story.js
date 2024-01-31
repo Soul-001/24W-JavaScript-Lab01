6dd83231becec84c3663a603ae87a806a44d7d74
Saharsh Patel <133163541+Soul-001@users.noreply.github.com>
2024-01-30 23:07:10 -0500

    Add files via upload

---
+++ js/silly-story.js
@@ -0,0 +1,53 @@
+// Edited by Saharsh Viren Patel #200520843
+// VARIABLE DECLARATIONS
+let customName = document.getElementById("customname");
+let randomize = document.querySelector(".randomize");
+let story = document.querySelector(".story");
+
+// STORY TEMPLATE
+let storyText =
+  "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+// ARRAYS
+let insertX = ["Donald Trump", "Jackie Chan", "Santa Claus"];
+let insertY = ["Area 51", "Death Valley", "Aruba"];
+let insertZ = [
+  "spontaneously combusted",
+  "rapidly sublimated",
+  "evaporated instantly",
+];
+
+// FUNCTIONS
+function randomValueFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function result() {
+  let newStory = storyText;
+
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(":insertx:", xItem);
+  newStory = newStory.replace(":inserty:", yItem);
+  newStory = newStory.replaceAll(":insertz:", zItem);
+
+  if (customName.value !== "") {
+    newStory = newStory.replaceAll("Bob", customName.value);
+  }
+
+  if (document.getElementById("metric").checked) {
+    let weight = Math.round(300 * 0.453592);
+    newStory = newStory.replace("300 pounds", weight + " kilograms");
+
+    let temp = Math.round(((94 - 32) * 5) / 9);
+    newStory = newStory.replace("94 fahrenheit", temp + " celsius");
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = "visible";
+}
+
+// EVENT LISTENERS
+randomize.addEventListener("click", result);
